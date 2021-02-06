e8497d252f406b95cd700d1a9326b15b88ec4c7e
Miroslav Genov <mgenov@gmail.com>
2021-02-06 11:12:50 +0200

    rpc/httpkit: improve doc comment of EncodeError

--- clouwayapis/rpc/httpkit/errors.go
+++ clouwayapis/rpc/httpkit/errors.go
@@ -16,10 +16,11 @@ import (
 const contentType = "application/json; charset=utf-8"
 
 // EncodeError writes the error to the ResponseWriter, by default a content
-// type of application/json, a body of json with key "error" and the value
+// type of application/json, a body of json with key "message" and the value
 // error.Error(), and a status code of 500. If the error implements Headerer,
 // the provided headers will be applied to the response. If the error
-// implements json.Marshaler, and the marshaling succeeds, the JSON encoded
+// implements grpc status.Error then the message and details will be encoded
+// as json and will be encoded otherwise json.Marshaler, and the marshaling succeeds, the JSON encoded
 // form of the error will be used. If the error implements StatusCoder, the
 // provided StatusCode will be used instead of 500.
 func EncodeError(_ context.Context, err error, w http.ResponseWriter) {
